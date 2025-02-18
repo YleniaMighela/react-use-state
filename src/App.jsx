f3c85056396010cbe6f12602382a0d25e41c6c68
Ylenia <yleniamighela@gmail.com>
2025-02-18 11:56:07 +0100

    added Main

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 import Header from "./components/Header.jsx"
+import Main from "./components/Main.jsx"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <>
 
             <Header />
+            <Main />
 
 
         </>
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main() {
+
+    return (
+        <main>
+            <p>questo è il main</p>
+        </main>
+    )
+};
